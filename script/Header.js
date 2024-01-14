0fee302d5e803dbc1eba408f61455b020715434f
Keshav Priyam <keshavpriyam@gmail.com>
2024-01-14 13:38:41 +0530

    update quiz and theme

--- script/Header.js
+++ script/Header.js
@@ -1,19 +1,19 @@
 const generateHeader = () => {
     return `
     <header class="text-center py-8 flex justify-around content-start flex-col lg:flex-row">
-            <a href="./index.html"><img src="./public/img/srm-logo.png" alt="Club Logo" class="mx-auto max-w-xs"></a>
+            <a href="./index.html"><img src="./public/img/srm-logo.png" alt="Club Logo" class="mx-auto w-48 md:w-64 md:max-w-xs"></a>
             <div class="mr-8 text-center hidden lg:block">
                 <h1 class="mt-6 text-5xl font-bold">IT Club</h1>
                 <p class="mt-1 text-2xl my-4">Department of Computer Applications</p>
             </div>
-            <a href="https://www.srmup.in/"><img id="club-logo" src="./public/img/iic-logo.png" alt="Club Logo" class="mx-auto h-32 w-32 max-w-xs mt-6 lg:mt-0"></a>
+            <a href="https://www.srmup.in/"><img id="club-logo" src="./public/img/logo.png" alt="Club Logo" class="mx-auto h-28 w-28 max-w-xs mt-6 lg:mt-0 rounded-full"></a>
             <div class="text-center block lg:hidden">
-                <h1 class="mt-6 text-5xl font-bold">IT Club</h1>
-                <p class="mt-1 text-xl my-4">Department of Computer Applications</p>
+                <h1 class="mt-6 text-3xl font-bold">IT Club</h1>
+                <p class="mt-1 text-lg my-4">Department of Computer Applications</p>
             </div>
     </header>
 
-    <nav class="bg-yellow-300 p-4 flex justify-evenly text-lg font-medium">
+    <nav class="bg-white p-4 flex justify-evenly text-lg font-medium border-b-2 shadow-md">
         <a class="mx-4" href="./index.html">Home</a>
         <a class="mx-4" href="./teams.html">Teams</a>
         <a class="mx-4" href="./members.html">Members</a>
