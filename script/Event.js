413dcaf5003d07bc6fcc50d3b329ffe71a2c815f
Keshav Priyam <keshavpriyam@gmail.com>
2024-01-20 22:19:59 +0530

    pic update and event heading

--- script/Event.js
+++ script/Event.js
@@ -8,9 +8,9 @@ const generateEvent = () =>{
         <div class="block md:grid grid-cols-3 items-center gap-12">
             <img src="./public/quizmantra.gif" alt="quizmantra" controls autoplay loop class="md:h-72 md:w-72 rounded-lg mx-auto shadow-lg shadow-black">
             <div class="text-center my-10">
-                <h1 class="text-4xl md:text-6xl font-bold mb-4 ">Join Us for an Exciting Event!</h1>
+                <h1 class="text-4xl md:text-6xl font-bold mb-4 ">On 23rd January!</h1>
                 <p class="text-lg md:text-xl mb-8">Be part of something amazing.</p>
-                <a href="https://forms.gle/HJWj87pykjT1fNwq7" target="_blank" class="cursor-pointer bg-blue-700 text-white text-3xl font-medium p-4 rounded-full text-xl">Register Now !</a>
+                <a href="https://forms.gle/HJWj87pykjT1fNwq7" target="_blank" class="pointer-events-none bg-red-700 text-white text-3xl font-medium p-4 rounded-full text-xl">Registrations Over</a>
             </div>
             <img src="./public/img/qr.jpg" alt="qr" srcset="" class="h-48 w-48 md:h-72 md:w-72 rounded-lg mx-auto shadow-lg shadow-black">
         </div>
